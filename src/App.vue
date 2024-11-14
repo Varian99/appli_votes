3c2a922628eb62b3f5190ae53cb94b46f17a0c34
Varian99 <hugobertrand99@gmail.com>
2024-11-14 16:21:15 +0100

    ajout connexion utilisateur

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
-<script setup lang="ts">
-import Todos from './components/Todos.vue'
+<script setup>
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
+
+Amplify.configure(outputs);
 </script>
 
 <template>
-  <main>
-    <Todos />
-  </main>
+  <authenticator :initial-state="'signIn'" hide-sign-up>
+    <template v-slot="{ user, signOut }">
+      <div v-if="user">
+        <h1>Bienvenue, {{ user.username }} !</h1>
+        <button @click="signOut">Se déconnecter</button>
+      </div>
+      <div v-else>
+        <h2>Veuillez vous connecter pour accéder à l'application de vote.</h2>
+      </div>
+    </template>
+  </authenticator>
 </template>
-
