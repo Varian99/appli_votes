5ca9f11da4723d074de03c9757d9c6325455e172
Varian99 <hugobertrand99@gmail.com>
2024-11-15 15:07:54 +0100

    bug fixes

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,35 +1,34 @@
+<script>
+import { API } from 'aws-amplify';
+
+export default {
+  data() {
+    return {
+      user: null,
+    };
+  },
+  async created() {
+    try {
+      // Appel à l'API REST pour récupérer les informations de l'utilisateur
+      const response = await API.get('VoteAPI', '/getProfileInfo');
+      this.user = response;
+    } catch (error) {
+      console.error('Erreur lors de la récupération des données utilisateur :', error);
+    }
+  },
+};
+</script>
+
 <template>
+  <div>
+    <h2>Profil de l'utilisateur</h2>
     <div v-if="user">
-      <h1>Bienvenue, {{ user.firstName }} {{ user.lastName }}!</h1>
-      <p>Date de naissance : {{ user.birthdate }}</p>
+      <p>Nom : {{ user.nom }}</p>
+      <p>Prénom : {{ user.prenom }}</p>
+      <p>Date de naissance : {{ user.dateNaissance }}</p>
     </div>
     <div v-else>
       <p>Chargement des informations...</p>
     </div>
-  </template>
-  
-  <script>
-  import { API } from 'aws-amplify';
-  
-  export default {
-    data() {
-      return {
-        user: null,
-      };
-    },
-    async created() {
-      try {
-        // Appel à la fonction Lambda pour récupérer les informations de profil
-        const response = await API.get('getProfileInfo', '/VoteAPI'); // Assurez-vous que l'API est bien configurée
-        this.user = response;
-      } catch (error) {
-        console.error('Erreur lors de la récupération des données utilisateur :', error);
-      }
-    },
-  };
-  </script>
-  
-  <style scoped>
-  /* Ajoutez ici des styles spécifiques au composant */
-  </style>
-  
+  </div>
+</template>
